71d6f414713e1a7af81a4a9528555b307eaccad6
mhmd mhdi <mhdimhdin5@gmail.com>
2024-12-29 14:03:17 +0330

    fix App.jsx as checkout.jsx address

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import ProductList from './components/ProductList.jsx';
 import Cart from './components/Cart.jsx';
-import Checkout from './components/Checkout.jsx';
+import Checkout from './components/checkout.jsx';
 
 function App() {
   return (
